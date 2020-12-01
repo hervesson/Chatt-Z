e1cbff960a110ce04937e695015273bcdbb233b2
hervesson <hervessonporto@hotmail.com>
2020-12-01 17:33:15 -0300

    Configuraçao firebase

--- src/App.js
+++ src/App.js
@@ -8,24 +8,24 @@ import "./assets/scss/themes.scss";
 import fakeBackend from './helpers/fake-backend';
 
 // //Firebase helper
-// import { initFirebaseBackend } from "./helpers/firebase";
+ import { initFirebaseBackend } from "./helpers/firebase";
 
 // TODO
 fakeBackend();
 
-// const firebaseConfig = {
-// 	apiKey: process.env.REACT_APP_APIKEY,
-// 	authDomain: process.env.REACT_APP_AUTHDOMAIN,
-// 	databaseURL: process.env.REACT_APP_DATABASEURL,
-// 	projectId: process.env.REACT_APP_PROJECTID,
-// 	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-// 	messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-// 	appId: process.env.REACT_APP_APPID,
-// 	measurementId: process.env.REACT_APP_MEASUREMENTID,
-// };
+ const firebaseConfig = {
+ 	apiKey: process.env.REACT_APP_APIKEY, 
+ 	authDomain: process.env.REACT_APP_AUTHDOMAIN,
+ 	databaseURL: process.env.REACT_APP_DATABASEURL,
+ 	projectId: process.env.REACT_APP_PROJECTID,
+ 	storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+ 	messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+ 	appId: process.env.REACT_APP_APPID,
+ 	measurementId: process.env.REACT_APP_MEASUREMENTID,
+ };
   
 // // init firebase backend
-// initFirebaseBackend(firebaseConfig);
+ initFirebaseBackend(firebaseConfig);
 
 function App() {
   return (
